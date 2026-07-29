3e3e4d5f57bdaf918e9ff145e8050366193b794a
AhmeeD. <41522322+a5q16@users.noreply.github.com>
2026-07-29 13:49:22 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,6 +49,22 @@ export default function RootLayout({
     <html lang="ar" dir="rtl" className={`${tajawal.variable} antialiased`}>
       <head>
         <meta name="facebook-domain-verification" content="gwcqzdqjy5f90gxktv5y9ljeqx7wzy" />
+        <script
+  dangerouslySetInnerHTML={{
+    __html: `
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', '1709205463738570');
+      fbq('track', 'PageView');
+    `,
+  }}
+/>
       </head>
       <body className="min-h-dvh bg-background text-foreground font-sans">
         {children}
